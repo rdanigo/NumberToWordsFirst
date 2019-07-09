aed9a1138655c208f7db64e16b5d4be6508382ab
Ruth Gomez <rdaniellagomez02@gmail.com>
2019-07-09 01:16:39 -0400

    Project Files Uploaded

---
+++ NumberToWordsTest/Class1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToWordsTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void numberEmptyReturnsException()
+        {
+            int? number = 2;
+            if (number == null)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public string int0returnStringZero()
+        {
+            int number = 0;
+            if (number == 0)
+            {
+                return "Zero";
+            }
+            return ""; 
+        }
+    }
+}
